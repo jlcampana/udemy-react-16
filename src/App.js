aff7f50f10a36964fdaaa94b1cd8a0fedff2b18e
Jose Luis Campaña Perez <jose-luis.campana.altran@axa-groupsolutions.com>
2018-11-12 14:12:04 +0100

    Style and some delegates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,62 @@ import React, { Component } from 'react'
 import './App.css'
 import Person from './components/Person/Person'
 
+const persons = [
+  { name: 'Jose Luis', age: 41 },
+  { name: 'Elle McPherson', age: 54 }
+]
+
 class App extends Component {
+  state = {
+    persons,
+    visible: true
+  }
+
+  nameChangeHandler = event => {
+    console.log('change')
+    this.setState({
+      persons: persons.map(i => {
+        return {
+          name: event.target.value,
+          age: i.age
+        }
+      })
+    })
+  }
+
+  fistreHandler = idx => {
+    persons[idx].name = 'Es un fistro'
+    this.setState({ persons })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
-        <Person />
+        <button style={style} onClick={this.fistreHandler.bind(this, 0)}>
+          Fistrea
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          delegate={this.fistreHandler.bind(this, 0)}
+          updateDelegate={this.nameChangeHandler}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          delegate={this.fistreHandler.bind(this, 1)}
+          updateNameDelegate={this.nameChangeHandler}
+        >
+          My hobbies: modelling
+        </Person>
       </div>
     )
   }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,7 +1,22 @@
+/**
+ * @class
+ * Stateless component type
+ * Utilizar siempre que se pueda!
+ * No manipula el state de la aplicación
+ */
 import React from 'react'
+import './Person.css'
 
-const person = () => {
-  return <p>Soy una persona</p>
+const person = props => {
+  return (
+    <div className="Person">
+      <p onClick={props.delegate}>
+        Me llamo {props.name} y tengo {props.age} años.
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.updateDelegate} value={props.name} />
+    </div>
+  )
 }
 
 export default person
