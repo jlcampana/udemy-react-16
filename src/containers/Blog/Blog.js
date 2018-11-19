7dc421430ea3fbef917febce2c90ad290019f38b
Jose Luis Campaña Perez <jose-luis.campana.altran@axa-groupsolutions.com>
2018-11-19 12:18:49 +0100

    Added mocked service for posts (Blog container)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+import './Blog.css'
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    //No sería mejor crear servicios y llamarlos desde este método?????
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+  render() {
+    return (
+      <div>
+        <section className="Posts">
+          <Post />
+          <Post />
+          <Post />
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    )
+  }
 }
 
-export default Blog;
+export default Blog
