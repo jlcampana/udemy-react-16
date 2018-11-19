e8b4d48d97972bd7744c4bed710ea1b1ca566193
Jose Luis Campaña Perez <jose-luis.campana.altran@axa-groupsolutions.com>
2018-11-19 13:04:34 +0100

    Adding interceptos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import axios from 'axios'
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
+axios.interceptors.request.use(
+  req => {
+    console.log('request interceptor', req)
+    return req
+  },
+  error => {
+    console.error('ERE UN FISTRO', error)
+    return Promise.reject(error)
+  }
+)
+
+axios.interceptors.response.use(
+  res => {
+    console.log(res)
+    return res
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
