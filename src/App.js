7ac89d04905ac8151f188d7de617ad4be0e3aa80
Jose Luis Campaña Perez <jose-luis.campana.altran@axa-groupsolutions.com>
2018-11-19 13:41:12 +0100

    Initial project for route

--- src/App.js
+++ src/App.js
@@ -1,101 +1,15 @@
-import React, { Component } from 'react'
-import classes from './App.css'
-import Person from './components/Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
-const persons = [
-  { name: 'Jose Luis', age: 41, id: 'fistro1' },
-  { name: 'Elle McPherson', age: 54, id: 'fistro2' }
-]
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-  state = {
-    persons,
-    visible: false
-  }
-
-  nameChangeHandler = (event, id) => {
-    console.log(id, event.target.value)
-    const personIdx = this.state.persons.findIndex(p => p.id === id)
-    const person = { ...this.state.persons[personIdx] }
-    person.name = event.target.value
-
-    const personList = [...this.state.persons]
-    personList[personIdx] = person
-    this.setState({ persons: personList })
-  }
-
-  fistreHandler = idx => {
-    persons[idx].name = 'Es un fistro'
-    this.setState({ persons })
-  }
-
-  toggleHandler = () => {
-    this.setState({ visible: !this.state.visible })
-  }
-
-  deletePersonHandler = idx => {
-    const tmpPersons = [...this.state.persons]
-    tmpPersons.splice(idx, 1)
-    this.setState({ persons: tmpPersons })
-  }
-
   render() {
-    let btnClass = ''
-    // const style = {
-    //   backgroundColor: 'white',
-    //   font: 'inherit',
-    //   border: '1x solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer'
-    // }
-
-    let personRender = null
-    if (this.state.visible) {
-      personRender = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                name={person.name}
-                age={person.age}
-                delegate={() => this.deletePersonHandler(index)}
-                key={person.id}
-                updateDelegate={event =>
-                  this.nameChangeHandler(event, person.id)
-                }
-              />
-            )
-          })}
-        </div>
-      )
-      btnClass = classes.Red
-    }
-
-    const assignedClasses = []
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.red)
-    }
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.bold)
-    }
-
     return (
-      <ErrorBoundary>
-        <div className={classes.App}>
-          <h1>Hi, I'm a react app</h1>
-          <p className={assignedClasses.join(' ')}>Fistros</p>
-          {/* <button style={style} onClick={this.fistreHandler.bind(this, 0)}>
-          Fistrea
-        </button> */}
-          <button onClick={this.toggleHandler} className={btnClass}>
-            Toggle
-          </button>
-          {personRender}
-        </div>
-      </ErrorBoundary>
-    )
+      <div className="App">
+        <Blog />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
